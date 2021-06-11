4a0d9399505851f335541218f5cb65393efc0c82
AKireenkov <kireeshka32@gmail.com>
2021-06-11 14:41:05 +0300

    SOAP простой пример

---
+++ soap-sample/src/test/java/ru/stqa/test/soap/GeoIpServiceTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.test.soap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+  @Test
+  public void testMyIp(){
+    //GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("37.147.38.237");
+   // assertEquals(geoIP.getCountryCode(), "RUS");
+  }
+}
