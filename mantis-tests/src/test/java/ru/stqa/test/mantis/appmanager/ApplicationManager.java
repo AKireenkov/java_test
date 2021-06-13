53525bc4f2ae5432d0a40c62320b60fbaa189c01
AKireenkov <kireeshka32@gmail.com>
2021-06-13 19:40:56 +0300

    SOAP: взаимодействие с MantisBT

--- mantis-tests/src/test/java/ru/stqa/test/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/test/mantis/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
   private MailHelper mailHelper;
   private JamesHelper jamesHelper;
   private YandexHelper yandexHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -96,5 +97,12 @@ public class ApplicationManager {
       yandexHelper = new YandexHelper(this);
     }
     return yandexHelper;
+  }
+
+  public SoapHelper soap() {
+    if (soapHelper == null) {
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
   }
 }
